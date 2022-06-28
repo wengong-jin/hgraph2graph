cbb4e42366e65e17809554cef73000882a8b0638
Muammar El Khatib <muammar.elkhatib@bms.com>
2022-06-28 09:54:42 -0400

    Proper change to finetune to work with newer chemprop
    See: https://github.com/chemprop/chemprop/issues/298

--- finetune_generator.py
+++ finetune_generator.py
@@ -35,9 +35,14 @@ class Chemprop(object):
             for fname in files:
                 if fname.endswith(".pt"):
                     fname = os.path.join(root, fname)
-                    # scaler, features_scaler = load_scalers(fname)
-                    # self.scalers.append(scaler)
-                    # self.features_scalers.append(features_scaler)
+                    (
+                        scaler,
+                        features_scaler,
+                        unused_scaler_1,
+                        unused_scaler_2,
+                    ) = load_scalers(fname)
+                    self.scalers.append(scaler)
+                    self.features_scalers.append(features_scaler)
                     model = load_checkpoint(fname)
                     self.checkpoints.append(model)
 
